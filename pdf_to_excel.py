3aabf914d33ff018f07b4772f03b90cca4ca66db
legnar <rythin@gmail.com>
2025-04-04 10:22:51 +0200

    Implemented batch conversions.

--- pdf_to_excel.py
+++ pdf_to_excel.py
@@ -1,5 +1,6 @@
 import tabula
 import pandas as pd
+import os
 
 
 def pdf_to_excel(pdf_file_path, excel_file_path):
@@ -12,4 +13,13 @@ def pdf_to_excel(pdf_file_path, excel_file_path):
             table.to_excel(writer, sheet_name=f'Sheet{i+1}')
 
 
-pdf_to_excel('path_to_pdf_file.pdf', 'path_to_excel_file.xlsx')
+# pdf_to_excel('pdf.pdf', 'path_to_excel_file.xlsx')
+PDF_FILES_DIR = 'pdf_files'
+EXCEL_FILES_DIR = 'excel_files'
+
+for filename in os.listdir(PDF_FILES_DIR):
+    pdfpath = os.path.join(PDF_FILES_DIR, filename)
+    filename = filename.split('.')[-2] + '.xlsx'
+    excelpath = os.path.join(EXCEL_FILES_DIR, filename)
+    pdf_to_excel(pdfpath, excelpath)
+    print(f'Converted {filename}.')
